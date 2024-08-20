df30605aaff2316b972d4bc4b17578b5bb69f363
AlexdeJesusFS <alexjesus0116@gmail.com>
2024-08-19 21:21:20 -0300

    Desafio Concluído.
    Atualizado para .NET8;
    Métodos e propriedades implementados;
    Testes realizados com sucesso.

---
+++ DesafioPOO/Program.cs
@@ -0,0 +1,14 @@
+﻿using DesafioPOO.Models;
+
+Nokia nokiaPhone = new("124578", "Unbreakable", "123456789012347", 4);
+Iphone iphonePhone = new("326598", "SEM", "123471234567890", 8);
+
+Console.WriteLine("Com Nokia: ");
+nokiaPhone.Ligar();
+nokiaPhone.ReceberLigacao();
+nokiaPhone.InstalarAplicativo("Block Buster");
+
+Console.WriteLine("\nCom Iphone: ");
+iphonePhone.Ligar();
+iphonePhone.ReceberLigacao();
+iphonePhone.InstalarAplicativo("Levie Ruve");
